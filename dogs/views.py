b671b012b9b9b22ac677d7014f029b4d4503bf97
proho <prohorovalex0013@gmail.com>
2025-04-02 00:38:05 +0400

    1. Написаны breeds_list view, breeds_dogs_list view; 2. Написаны url для breeds_list view, breeds_dogs_list view; 3.Добавлены шаблоны breeds.html, dogs.html

--- dogs/views.py
+++ dogs/views.py
@@ -9,4 +9,19 @@ def index(request):
     }
     return render(request, 'dogs/index.html', context)
 
+def breeds_list(request):
+    context = {
+        'objects_list': Breed.objects.all(),
+        'title': 'Питомник - Все наши породы'
+    }
+    return render(request, 'dogs/breeds.html', context)
+
+def breed_dogs_list(request, pk: int):
+    breed_item = Breed.objects.get(pk=pk)
+    context = {
+        'object_list': Dog.objects.filter(breed_id=pk),
+        'title': f'Собаки породы - {breed_item.name}',
+        'breed_pk': breed_item.pk,
+    }
+    return render(request, 'dogs/dogs.html', context)
 
