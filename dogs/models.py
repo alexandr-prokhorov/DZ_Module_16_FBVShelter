923aed89df63e8af106d146be80eb59b7bfc3264
proho <prohorovalex0013@gmail.com>
2025-04-01 18:52:08 +0400

    Добавлена модель Dog; Добавлена библиотека Pillow; Созданы и проведены миграции.

--- dogs/models.py
+++ dogs/models.py
@@ -12,3 +12,24 @@ class Breed(models.Model):
     class Meta:
         verbose_name = 'breed'
         verbose_name_plural = 'breeds'
+
+class Dog(models.Model):
+    name = models.CharField(max_length=250, verbose_name='dog_name')
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='breed')
+    photo = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='image')
+    birth_date = models.DateField(**NULLABLE, verbose_name='birth_date')
+
+    def __str__(self):
+        return f'{self.name} ({self.breed})'
+
+
+    class Meta:
+        verbose_name = 'dog'
+        verbose_name_plural = 'dogs'
+        # варианты работы с мета классом
+        # abstract = True
+        # app_label = 'dogs'
+        # ordering = [-1]
+        # permissions = []
+        # db_table = 'doggies'
+        # get_latest_by = 'birth_date'
