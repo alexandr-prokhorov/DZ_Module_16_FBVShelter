46a3be6ec4a8f4c8081317e34eb00346e3ee9d6d
proho <prohorovalex0013@gmail.com>
2025-04-12 18:31:54 +0400

    1) Откорректировал модели 2) Созданы и применены миграции

--- dogs/models.py
+++ dogs/models.py
@@ -3,8 +3,8 @@ from django.db import models
 from users.models import NULLABLE
 
 class Breed(models.Model):
-    name = models.CharField(max_length=100, verbose_name='breed')
-    description = models.CharField(max_length=1000, verbose_name='description', **NULLABLE)
+    name = models.CharField(max_length=100, verbose_name='Порода')
+    description = models.CharField(max_length=1000, verbose_name='Описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
@@ -14,10 +14,10 @@ class Breed(models.Model):
         verbose_name_plural = 'breeds'
 
 class Dog(models.Model):
-    name = models.CharField(max_length=250, verbose_name='dog_name')
-    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='breed')
-    photo = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='image')
-    birth_date = models.DateField(**NULLABLE, verbose_name='birth_date')
+    name = models.CharField(max_length=250, verbose_name='Кличка')
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='Порода')
+    photo = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='Фото')
+    birth_date = models.DateField(**NULLABLE, verbose_name='Дата рождения')
 
     def __str__(self):
         return f'{self.name} ({self.breed})'
--- users/models.py
+++ users/models.py
@@ -5,13 +5,13 @@ NULLABLE = {'blank': True, 'null': True}
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name='email')
-    first_name = models.CharField(max_length=150, verbose_name='first name', default='Anonymous')
-    last_name = models.CharField(max_length=150, verbose_name='last name', default='Anonymous')
-    phone = models.CharField(max_length=35, verbose_name='phone number', **NULLABLE)
-    telegram = models.CharField(max_length=150, verbose_name='telegram username', **NULLABLE)
-    avatar = models.ImageField(upload_to='users/', verbose_name='avatar', **NULLABLE)
-    is_active = models.BooleanField(default=True, verbose_name='active')
+    email = models.EmailField(unique=True, verbose_name='Эл.Почта')
+    first_name = models.CharField(max_length=150, verbose_name='Имя', default='Anonymous')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия', default='Anonymous')
+    phone = models.CharField(max_length=35, verbose_name='Номер Телефона', **NULLABLE)
+    telegram = models.CharField(max_length=150, verbose_name='Телеграм', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
