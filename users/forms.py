829cdb3ffa12952bbcfc128754158feb2932e2c2
proho <prohorovalex0013@gmail.com>
2025-04-12 18:34:00 +0400

    1) Написан StyleFormMixin 2) Добавлен к формам

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,13 @@ from django import forms
 
 from users.models import  User
 
-class UserRegisterForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+class UserRegisterForm(StyleFormMixin, forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
 
@@ -16,11 +22,11 @@ class UserRegisterForm(forms.ModelForm):
             raise forms.ValidationError('Пароли не совпадают!!!')
         return cd['password2']
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(StyleFormMixin, forms.Form):
     email = forms.EmailField()
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
 
-class UserForm(forms.ModelForm):
+class UserForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone',)
