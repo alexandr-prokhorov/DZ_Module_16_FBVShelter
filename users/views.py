b119facc1593776e7fac810db87ae6d937bdcff4
proho <prohorovalex0013@gmail.com>
2025-04-13 19:01:14 +0400

    1)Реализована отправка письма при регистрации. 2)Реализована возможность генерации пароля пользователя и отправка уведомления при изменении пароля

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.shortcuts import render, reverse, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
@@ -5,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm
+from users.services import send_new_password, send_register_email
 
 
 def user_register_view(request):
@@ -14,6 +18,7 @@ def user_register_view(request):
             new_user = form.save(commit=False)
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
+            send_register_email(new_user.email)
             login(request, new_user)
             return HttpResponseRedirect(reverse('dogs:index'))
     else:
@@ -99,3 +104,10 @@ def user_logout_view(request):
     logout(request)
     return redirect('dogs:index')
 
+@login_required
+def user_generate_new_password_view(request):
+    new_password = ''.join(random.sample((string.ascii_letters + string.digits), 12))
+    request.user.set_password(new_password)
+    request.user.save()
+    send_new_password(request.user.email, new_password)
+    return redirect(reverse('dogs:index'))
